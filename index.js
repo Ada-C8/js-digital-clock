97d27fb6f099128a6aea4e7bc1ba7c151674af47
VirtualLindsey <tapircode@gmail.com>
2017-11-15 22:06:12 -0800

    pushing questions for charles

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+$(document).ready(function () {
+  const thisTime = function() {
+    let today = new Date();
+    let h = today.getHours();
+    if (h >= 24) {
+      h = h - 24;
+    }
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    h = updateTime(h);
+    m = updateTime(m);
+    s = updateTime(s);
+    if (h > 12) {
+      return ((h-12) + ":" + m + ":" + s + " pm");
+    } else {
+      return (h + ":" + m + ":" + s + " am");
+    }
+  };
+
+  const updateTime = function (num) {
+    if (num < 10) {
+      num = "0" + num
+    };
+    return num;
+  };
+
+  $('#seattle').append(thisTime());
+
+  const currentClock = function (){
+    $('#seattle').html(thisTime());
+  };
+
+  setInterval(currentClock, 1000);
+});
