bf6c5eeb6e067bbb9b59723a30e91b683cccca43
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-11-17 15:15:35 -0800

    Time changes every second

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+const twoDigits = function twoDigits(time) {
+  let returnTime = time;
+  if (time < 10) {
+    returnTime = `0${time}`;
+  }
+  return returnTime;
+};
+
+const getTime = function getTime() {
+  const date = new Date(Date.now());
+  const hours = twoDigits(date.getHours());
+  const minutes = twoDigits(date.getMinutes());
+  const seconds = twoDigits(date.getSeconds());
+
+  $('#clock').html(`<span>${hours}:${minutes}:${seconds}</span>`);
+};
+
+let interval;
+const changeTime = function changeTime() {
+  interval = setInterval(getTime, 1000);
+};
