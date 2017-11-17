4d6b3c74950ef0ef6b52c503711a51d212e7bb80
Amy Cash <amymcash@gmail.com>
2017-11-17 15:27:31 -0800

    Set up basic time function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+
+$(document).ready(function() {
+  let d = new Date();
+  let h = d.getHours();
+  let m = d.getMinutes();
+  let s = d.getSeconds();
+  let t = h + ':' + m + ':' + s;
+  $('#clock').append(t);
+
+});
+
+
+// $('#clock').append(Date.now());
