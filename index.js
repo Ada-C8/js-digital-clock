54679676c713bb50e419daa59a57a318f7fff0cd
Eva Sawyer <esawyer25@gmail.com>
2017-11-17 15:40:28 -0800

    working version

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Your code here
+console.log('hello');
+
+
+// $('#clock').append('<h2> Hello World! </h2>');
+
+
+const updateClock = function updateClock() {
+  let currentTime = new Date ( );
+  let currentHours = currentTime.getHours ( );
+  // if (currentHours > 12) {
+  //   currentHours = currentHours - 12;
+  // };
+
+  let currentMinutes = currentTime.getMinutes ( );
+  if (currentMinutes < 10) {
+    currentMinutes = '0' + currentMinutes;
+  };
+
+  let currentSeconds = currentTime.getSeconds ( );
+  if (currentSeconds < 10) {
+    currentSeconds = '0' + currentSeconds;
+  };
+
+  let time = currentHours + ':' + currentMinutes + ':' + currentSeconds
+  return time;
+};
+//
+// const ellapsedTime = function ellapsedTime() {
+//   let currentTime = new Date ( );
+//   const party = new Date(Date.UTC(2009, 6, 28, 12, 0));
+//   let difference = currentTime - party;
+//   let partyHours = difference.getHours ( );
+//   let partyMinutes = difference.getMinutes ( );
+//   let partySeconds = difference.getSeconds ( );
+//   let timeToParty = partyHours + ':' + partyMinutes + ':' + partySeconds;
+//
+//   return timeToParty
+//
+//
+//
+// }
+
+
+
+
+$('document').ready(() => {
+  setInterval(function() {
+    $('#clock').text(updateClock, 1000);
+  })
+  //  $('#clock').append(ellapsedTime);
+});
+
+
+  // $('document').ready(() => {
+  //   $('#clock').append(updateClock);
+  //   $('#clock').setInterval;
+  // });
