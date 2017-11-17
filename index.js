00f255ce13e9dc0f4ec463d20ceec30d052cf6b9
Canaan West <canaan.west@gmail.com>
2017-11-17 15:54:17 -0800

    preliminary code for making 12 hour clock

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ $(document).ready(setInterval(function() {
   const hour = date.getHours();
   const minute = date.getMinutes();
   const second = date.getSeconds();
+  //
+  // if (hour > 12) {
+  //   hour -= 12
+  // }
+  //
+  // if (second < 10){
+  //   // second = `0${second}`
+  // }
+
+
   const timeNow = `${hour} : ${minute} : ${second}`;
 
   $('#clock').html(timeNow);
