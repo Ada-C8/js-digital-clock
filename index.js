3de31194fa7ab3c818426d9271b3ffcaa7a5dbcd
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-11-17 14:36:18 -0800

    Initial commit with basic clock functionality working.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+
+$(document).ready (function() {
+
+  console.log('here');
+  getTime();
+
+});
+
+const getTime = function getTime() {
+  setInterval(getTime, 1000);
+
+  let today = new Date();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  let seconds = today.getSeconds();
+  document.getElementById('clock').innerHTML =
+    hours + ":" + minutes + ":" + seconds;
+
+
+};
