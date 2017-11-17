a7c6774b36843b22af2acda58ebeb2a71439937e
Sarah Read-Brown <srbusiness2@gmail.com>
2017-11-17 15:19:30 -0800

    functioning basic clock

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+
+$(document).ready(()=>{
+  clock();
+})
+
+const clock = function clock(){
+  let d = new Date();
+  let local = d.toLocaleTimeString();
+  document.getElementById('clock').innerHTML=local;
+};
+
+setInterval(function(){clock()}, 1000);
