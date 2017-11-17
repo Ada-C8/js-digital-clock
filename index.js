06acd2746e7d44fe7558909439a1dfa3ab6c2088
Laura Robertson <mslaura.mae@gmail.com>
2017-11-17 15:45:52 -0800

    A functioning clock is displayed to a user

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+const seconds = 1000;
+
+const extraZero = num => `0${num}`.slice(-2);
+
+const coolClock = () => {
+  let currentTime = new Date();
+  const hour = currentTime.getHours();
+  const minute = currentTime.getMinutes();
+  const second = currentTime.getSeconds();
+
+  currentTime = `${extraZero(hour)}:${extraZero(minute)}:${extraZero(second)}`;
+
+  const displayClock = document.getElementById('clock');
+  displayClock.innerHTML = currentTime;
+};
+
+setInterval(coolClock, seconds);
