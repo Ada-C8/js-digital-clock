ec14e6f097da692ae1dfd50529a643e8f61b1766
Canaan West <canaan.west@gmail.com>
2017-11-17 15:36:26 -0800

    retrieving hours, minutes, seconds

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+
+const time = function time() {
+  const date = new Date();
+  hour = date.getHours()
+  minute = date.getMinutes()
+  second = date.getSeconds()
+  timeNow = `${hour} : ${minute} : ${second}`;
+}
+
+console.log(time());
