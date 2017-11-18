20132e35eaa427117beed6731579d9b7a357a0ad
Andrea Chen <andreachen.ac@gmail.com>
2017-11-18 07:53:03 -0800

    Typo:

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ $(document).ready(() => {
     let date = today.getDate();
 
     let months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-    let days = ['Sunday','Monday','Tueday','Wednesday','Thursday','Friday','Saturday'];
+    let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
     let month = months[today.getMonth()];
     let day = days[today.getDay()];
