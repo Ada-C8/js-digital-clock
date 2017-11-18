739b3219d35984039f5e9ebdeca59cd8be58c3f8
Shaunna <skwiens@gmail.com>
2017-11-18 09:47:22 -0800

    basic functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+
+$(document).ready(() => {
+  const readTime = () => {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    let hours = now.getHours();
+    let m = 'AM';
+
+    if (hours > 12) {
+      m = 'PM';
+      hours -= 12;
+    }
+
+    return `<p>${hours}:${minutes}:${seconds} ${m}</p>`;
+  };
+
+  setInterval(() => {
+    $('#clock').empty();
+    $('#clock').append(readTime);
+  }, 1000);
+});
