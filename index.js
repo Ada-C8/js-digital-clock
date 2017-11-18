265dbaa8151271287914d64482b306dc48df3d3c
Angela Wilson <wilsonange@gmail.com>
2017-11-18 12:40:23 -0800

    add comments with English translation

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ const clock = function clock(){
   let today = new Date();
   let locale = today.toLocaleTimeString('ko-KR', { hour12: false});
   document.getElementById('clock').innerHTML = '시애틀 시간은 한국으로<br>' + locale;
+  // 시애틀 시간은 한국으로 translates to Seattle's time in Korean
 };
