55d4163dc92698f1a7464810aaf17432762544b9
Eva Sawyer <esawyer25@gmail.com>
2017-11-17 21:32:52 -0800

    added extras

--- index.js
+++ index.js
@@ -3,56 +3,71 @@ console.log('hello');
 
 
 // $('#clock').append('<h2> Hello World! </h2>');
+const updateDate = function updateDate() {
+  const currentTime = new Date();
+  const currentDay = currentTime.getDate();
+  const currentMonth = currentTime.getMonth();
+  const currentYear = currentTime.getFullYear();
+  return `${currentDay} / ${currentMonth} / ${currentYear}`
+}
 
 
 const updateClock = function updateClock() {
-  let currentTime = new Date ( );
-  let currentHours = currentTime.getHours ( );
-  // if (currentHours > 12) {
-  //   currentHours = currentHours - 12;
-  // };
+  const currentTime = new Date();
+  let currentHours = currentTime.getHours();
+  let timeOfDay = 'AM'
+  if (currentHours > 12) {
+    currentHours = currentHours - 12;
+    timeOfDay = 'PM'
+  };
 
-  let currentMinutes = currentTime.getMinutes ( );
+  let currentMinutes = currentTime.getMinutes();
   if (currentMinutes < 10) {
     currentMinutes = '0' + currentMinutes;
   };
 
-  let currentSeconds = currentTime.getSeconds ( );
+  let currentSeconds = currentTime.getSeconds();
   if (currentSeconds < 10) {
     currentSeconds = '0' + currentSeconds;
   };
 
-  let time = currentHours + ':' + currentMinutes + ':' + currentSeconds
+  let time = `${currentHours}: ${currentMinutes}: ${currentSeconds} ${timeOfDay}`;
   return time;
 };
-//
-// const ellapsedTime = function ellapsedTime() {
-//   let currentTime = new Date ( );
-//   const party = new Date(Date.UTC(2009, 6, 28, 12, 0));
-//   let difference = currentTime - party;
-//   let partyHours = difference.getHours ( );
-//   let partyMinutes = difference.getMinutes ( );
-//   let partySeconds = difference.getSeconds ( );
-//   let timeToParty = partyHours + ':' + partyMinutes + ':' + partySeconds;
-//
-//   return timeToParty
-//
-//
-//
-// }
 
-
+const ellapsedTime = function ellapsedTime() {
+  let currentTime = new Date ( );
+  const party = new Date(Date.UTC(2009, 6, 28, 12, 0));
+  let diff = Math.round((currentTime - party) / 1000);
+  let y = Math.floor(diff/(365.25*24*60*60));
+  diff = diff-(y*365.25*24*60*60);
+  let d = Math.floor(diff/(24*60*60));
+  diff = diff-(d*24*60*60)
+  var h = Math.floor(diff/(60*60));
+  diff = diff-(h*60*60);
+  var m = Math.floor(diff/(60));
+  diff = diff-(m*60);
+  var s = Math.floor(diff);
+  let timeToParty = 'Years: ' + y + '\nDays: '+ d + '\n Hours: ' + h + '\n Mins: ' + m + '\nSeconds:' + s;
+  return timeToParty;
+};
 
 
 $('document').ready(() => {
   setInterval(function() {
-    $('#clock').text(updateClock, 1000);
-  })
-  //  $('#clock').append(ellapsedTime);
+    $('#currentDate').text(updateDate, 1000);
+  });
+});
+
+$('document').ready(() => {
+  setInterval(function() {
+    $('#currentTime').text(updateClock, 1000);
+  });
 });
 
 
-  // $('document').ready(() => {
-  //   $('#clock').append(updateClock);
-  //   $('#clock').setInterval;
-  // });
+$('document').ready(() => {
+  setInterval(function() {
+    $('#time-travel').text(ellapsedTime, 1000);
+  });
+});
