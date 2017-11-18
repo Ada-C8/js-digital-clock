2c041674ff66b1cade82d5f23ad9468b5cf3278c
Jan Edrozo <jedrozo@gmail.com>
2017-11-17 21:00:28 -0800

    Added date and timer js along with date.js file

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+
+const date = document.getElementById('date');
+const time = document.getElementById('time');
+
+function getDate() {
+  date.innerHTML = Date.today().toString('MMMM d, yyyy');
+}
+
+function timer() {
+  const d = new Date();
+  setInterval(timer, 1000);
+  time.innerHTML = d.toLocaleTimeString();
+}
+
+getDate();
+timer();
+
+// NOTES:
+// setInterval(function name, frequency 1000ms = 1 second);
