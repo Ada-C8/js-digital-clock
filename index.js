4596d58a8abc73c797bb1370ca5f9ef1a94d3d4a
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-11-17 20:55:43 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code here
+
+$(document).ready(function() {
+
+  function update () {
+    let date = new Date();
+    $('#clock').text(date.toLocaleTimeString());
+    }
+    window.setInterval(update, 1000);
+  }
+);
