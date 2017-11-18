b6fede93878de524c6fde2b336fe995baa3263a5
Severin Wiggenhorn <severin.randall@gmail.com>
2017-11-18 02:37:32 -0800

    reformatted how minutes and seconds display

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const myVar = setInterval(function(){ clock() }, 1000);
+const myVar2 = setInterval(function(){ todayDate() }, 1000);
+
 const clock = function digitalClock() {
   let now = new Date();
-  console.log(now);
   let hours = now.getHours();
   let minutes = now.getMinutes();
   let seconds = now.getSeconds();
-  // minutes = minutes.checkTime();
-  // seconds = seconds.checkTime();
+  minutes = checkTime(minutes);
+  seconds = checkTime(seconds);
   document.getElementById('clock').innerHTML =
   hours + ":" + minutes + ":" + seconds;
   var t = setTimeout(clock, 500);
@@ -14,23 +16,11 @@ const clock = function digitalClock() {
 function checkTime(i) {
   if (i < 10) { i = '0' + i };  // add zero in front of numbers < 10
   return i;
-}
-
-// var myVar = setInterval(function(){ clock() }, 1000);
+};
 
 const todayDate = function todayDate () {
   let now = new Date();
   now = now.toDateString();
-  console.log(now)
-  // let day = now.getDay();
-  // let month = now.getHours();
-  // let dateNum = now.getDate();
-  // let year = now.getFullYear();
   document.getElementById('todayDate').innerHTML =
   now;
-}
-// $(document).ready(() => {
-// var clockDiv = document.getElementById('clock');
-// clockDiv.innerHTML = `<h1>${hours} : ${minutes} : ${seconds}</h1>`;
-// $('.clock').append(clock());
-// });
+};
