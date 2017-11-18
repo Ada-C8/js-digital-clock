e4e6c12848e0bddbd5b633eabd8776934e39eba1
Angela Wilson <wilsonange@gmail.com>
2017-11-18 11:10:42 -0800

    digital clock complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code here
+$(document).ready(()=>{
+  setInterval(clock, 1000);
+})
+
+const clock = function clock(){
+  let today = new Date();
+  let locale = today.toLocaleTimeString('ko-KR', { hour12: false});
+  document.getElementById('clock').innerHTML = '시애틀 시간은 한국으로<br>' + locale;
+};
