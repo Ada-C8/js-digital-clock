e7d08975265708725f0ff0476b06c1e4d7b8e9bf
Canaan West <canaan.west@gmail.com>
2017-11-17 16:18:59 -0800

    changing to localedatetime

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 // Your code here
 $(document).ready(setInterval(function() {
-  const date = new Date();
-  const hour = date.getHours();
-  const minute = date.getMinutes();
-  const second = date.getSeconds();
-  //
-  // if (hour > 12) {
-  //   hour -= 12
-  // }
-  //
-  // if (second < 10){
-  //   // second = `0${second}`
-  // }
+  const date = new Date().toLocaleTimeString();
+  // const hour = date.getHours();
+  // const minute = date.getMinutes();
+  // const second = date.getSeconds();
 
 
-  const timeNow = `${hour} : ${minute} : ${second}`;
+  // const timeNow = `${hour} : ${minute} : ${second}`;
 
-  $('#clock').html(timeNow);
+  $('#clock').html(date);
 }, 1000));
