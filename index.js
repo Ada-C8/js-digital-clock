f44b604cb5b0779345488a5f5d3d1bc699f70793
Jessica Owens <vertige@gmail.com>
2017-11-17 23:19:17 -0800

    created displayClock function and implemented a window.setInterval to output via jQuery

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-// Your code here
-const time = new Date().toLocaleTimeString();
-console.log(time);
+$(document).ready(() => {
+  const displayClock = function displayClock() {
+    return new Date(Date.now()).toLocaleTimeString();
+  };
+  window.setInterval(() => { $('#clock').html(displayClock()); }, 1000);
+});
