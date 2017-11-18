ad039252d5536eceafee15c1a727bf505a7aa1ac
averikitsch <aekitsch@gmail.com>
2017-11-18 13:09:54 -0800

    added interval and changed styling

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+
+var intervalID = window.setInterval(myClock, 1000);
+
+Number.prototype.pad = function(size) {
+  var s = String(this);
+  while (s.length < (size || 2)) {s = "0" + s;}
+  return s;
+}
+
+function myClock() {
+  // let time = new Date().toLocaleTimeString();
+  // $('#clock').text(time);
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let seconds = time.getSeconds();
+
+  $('#hour').text(hour);
+  $('#minutes').text(min.pad());
+  $('#seconds').text(seconds.pad());
+}
+
+function myBackground() {
+
+}
