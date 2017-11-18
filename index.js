77ca46f184c214d4c9d7a91e05b688d6239e43e1
Mariana <lmarianarp@gmail.com>
2017-11-18 15:11:23 -0800

    Digital Clock

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+
+$(document).ready(()=>{
+  const clock = function clock() {
+      let date = new Date();
+      let hours = date.getHours();
+      let minutes = date.getMinutes();
+      let seconds = date.getSeconds();
+      let m = 'M';
+
+      m = (hours < 12) ? 'A'+m : 'P' + m;
+      hours = (hours === 0 ) ? 12 : hours;
+      hours = (hours > 12) ?  hours - 12 : hours;
+      // hours = (hours < 10) ? '0' + hours : hours;
+      // hours = (hours < 10) ? '0' + hours : hours;
+      minutes = (minutes < 10) ? '0' + minutes : minutes;
+      seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+      let time =  hours + ':'+ minutes + ':' + seconds + m;
+      document.getElementById('clock').innerText = time;
+    };
+    clock();
+    setInterval(clock, 1000);
+
+    $('#clock').append(clock());
+
+  });
