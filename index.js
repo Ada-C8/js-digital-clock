eaf94ee9460bc04c7bfef31e488b271f1cc721c0
Canaan West <canaan.west@gmail.com>
2017-11-18 09:06:50 -0800

    taking out unused vars

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 // Your code here
 $(document).ready(setInterval(function() {
   const date = new Date().toLocaleTimeString();
-  // const hour = date.getHours();
-  // const minute = date.getMinutes();
-  // const second = date.getSeconds();
-
-
-  // const timeNow = `${hour} : ${minute} : ${second}`;
 
   $('#clock').html(date);
 }, 1000));
