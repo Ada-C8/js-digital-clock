c0939e3f479c4e930987f1cd43ebfb2223d30ba8
Angela Wilson <wilsonange@gmail.com>
2017-11-18 14:22:41 -0800

    clock complete working to deploy to heroku

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code here
+$(document).ready(()=>{
+  setInterval(clock, 1000);
+})
+
+const clock = function clock(){
+  let today = new Date();
+  let locale = today.toLocaleTimeString('ko-KR', { hour12: false});
+  document.getElementById('clock').innerHTML = '시애틀 시간은 한국으로<br>' + locale;
+  // 시애틀 시간은 한국으로 translates to "Seattle's time in Korean"
+};
