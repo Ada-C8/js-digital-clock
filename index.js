41e339149ce4ad3ba15458d3b3901c4359f7ade9
Stef <seedsofthesesame@gmail.com>
2017-11-17 22:37:39 -0800

    Completed base requirements of hour, minutes, and seconds of local time.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code here
+$(document).ready(function() {
+  function clock (){
+    let date = new Date();
+    $('#clock').text(date.toLocaleTimeString() );
+  }
+  window.setInterval(clock, 1000)
+})
