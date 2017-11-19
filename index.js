6083f280b08644febede7115d33105093a74bac4
Amy Lee <amy.yj.lee@gmail.com>
2017-11-19 13:58:21 -0800

    manually adding times for seoul, lima, and berlin

--- index.js
+++ index.js
@@ -2,10 +2,24 @@
 const myClock = () => {
   let date = new Date();
   let timeString = date.toLocaleTimeString();
-  document.getElementById('clock').innerHTML = date.toLocaleTimeString();
+  const timeOptions = {
+    hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true,
+  };
+  // document.getElementById('local').innerHTML = date.toLocaleTimeString();
   $('.clock').html(`<p> ${timeString} <p>`);
-};
+  timeOptions.timeZone = 'Asia/Seoul';
+  const seoulTime = new Intl.DateTimeFormat('en-US', timeOptions).format(date);
+  $('#seoul').html(`<p> Current Time in Seoul, South Korea: ${seoulTime} <p>`);
 
+  timeOptions.timeZone = 'Europe/Berlin';
+  const berlinTime = new Intl.DateTimeFormat('en-US', timeOptions).format(date);
+  $('#berlin').html(`<p> Current Time in Berlin, Germany: ${berlinTime} <p>`);
+
+  timeOptions.timeZone = 'America/Lima';
+  const limaTime = new Intl.DateTimeFormat('en-US', timeOptions).format(date);
+  $('#lima').html(`<p> Current Time in Lima, Peru: ${limaTime} <p>`);
+
+};
 // const display = setInterval(myClock, 1000);
 
 // const fixTime = function fixTime(i) {
@@ -30,18 +44,27 @@ const myClock = () => {
 const display = setInterval(myClock, 1000);
 
 
-// below are display options, do not fetch the actual time in those places unless a timezone is provided
-const dateOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
+// $(.clock).ready(function() {
+//   for (let i = 0; i < $(.clock).length ; i++) {
+//     let listItem = $('<li>' + i + '</li>');
+//     $('.dynamic-list').append(listItem);
+//   }
+// });
 
-const timeOptions = {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true } ;
+// below are display options, do not fetch the actual time in those places unless a timezone is provided
+// const dateOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
+//
+// const timeOptions = {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true } ;
+//
+// timeOptions.timeZone =
 
 // must manually add timeZone to Options;
 
-const date = new Date();
-
-const dateKorea = new Intl.DateTimeFormat('ko-KR', dateOptions).format(date);
-
-const timeKorea = new Intl.DateTimeFormat('ko-KR', timeOptions).format(date);
+// const date = new Date();
+//
+// const dateKorea = new Intl.DateTimeFormat('ko-KR', dateOptions).format(date);
+//
+// const timeKorea = new Intl.DateTimeFormat('ko-KR', timeOptions).format(date);
 
 // Seoul: UTC/GMT +9 hours, 17 hours ahead of Seattle timeZone = 'Asia/Seoul'
 // Berlin: UTC/GMT +1 hour, 9 hours ahead of Seattle timeZone = 'Europe/Berlin'
