62d02de0c7c519b72bae99707c346c38b8d4c6fa
Amy Cash <amymcash@gmail.com>
2017-11-19 11:48:15 -0800

    Fix basic time function back to what it was

--- index.js
+++ index.js
@@ -1,15 +1,149 @@
-// Your code here
+/* THE FOLLOWING WILL ADD
+THE THING IN THE APPEND TO
+THE PAGE */
+// $(document).ready(function() {
+//   console.log("Something");
+//   $('#clock').append("SOMETHING");
+// })
 
+/*The Following will both add a time in a
+not parsed format */
+//
+// $(document).ready(function() {
+//   $('#clock').append( Date.now() );
+// })
+
+// $(document).ready(function() {
+//   let d = Date.now();
+//   $('#clock').append(d);
+// })
+
+/* This will put the current date in a parsed by LONG format */
+// $(document).ready(function() {
+//   let d = new Date();
+//   let h = d.getHours();
+//   console.log(h);
+//   $('#clock').append(d);
+// })
+
+/* This is will put the time
+formatted as a digital clock
+except seconds is only 1 digit */
 
 $(document).ready(function() {
   let d = new Date();
   let h = d.getHours();
   let m = d.getMinutes();
   let s = d.getSeconds();
-  let t = h + ':' + m + ':' + s;
+  let t = (h + ':' + m + ':' + s);
   $('#clock').append(t);
+})
 
-});
 
 
-// $('#clock').append(Date.now());
+//
+// Your code here
+// const setDate = function setDate() {
+//   const now = new Date();
+//   const seconds = now.getSeconds();
+//   const mins = now.getMinutes();
+//   const hour = now.getHours();
+//
+//   setInterval(setDate, 1000);
+// });
+
+// $(document).ready(function() {
+//
+//   const setDate = function(setDate() {
+//     const now = new Date();
+//     const seconds = now.getSeconds();
+//     const mins = now.getMinutes();
+//     const hour = now.getHours();
+//     const date = seconds + mins + hour;
+//     // setInterval(setDate, 1000);
+//   });
+//
+//     $('#clock').append(date;
+//   });
+
+// });
+
+// $(document).ready(function ()) {
+//   $('#clock').append("hi there");
+// }
+//
+// $(document).ready(function() {
+
+// (hours + ":" + minutes + ":" + seconds + meridiem);
+// </script>
+
+// $(document).ready(function() {
+//   let interval = setInterval(myTimer, delay[1000]);
+//
+//   function myTimer() {
+//     let d = new Date();
+//     let t = d.getSeconds();
+//   }
+//   // let d = new Date();
+//   // let h = d.getHours();
+//   // let m = d.getMinutes();
+//   // let s = d.getSeconds();
+//   // let t = h + ':' + m + ':' + s;
+//   $('#clock').append(interval);
+//
+// });
+
+
+
+// function clock(){
+//
+// //Save the times in variables
+//
+// var today = new Date();
+//
+// var hours = today.getHours();
+// var minutes = today.getMinutes();
+// var seconds = today.getSeconds();
+//
+//
+// //Set the AM or PM time
+//
+// if (hours >= 12){
+//   meridiem = " PM";
+// }
+// else {
+//   meridiem = " AM";
+// }
+//
+//
+// //convert hours to 12 hour format and put 0 in front
+// if (hours>12){
+// 	hours = hours - 12;
+// }
+// else if (hours===0){
+// 	hours = 12;
+// }
+//
+// //Put 0 in front of single digit minutes and seconds
+//
+// if (minutes<10){
+// 	minutes = "0" + minutes;
+// }
+// else {
+// 	minutes = minutes;
+// }
+//
+// if (seconds<10){
+// 	seconds = "0" + seconds;
+// }
+// else {
+// 	seconds = seconds;
+// }
+//
+//
+// document.getElementById("clock").innerHTML = (hours + ":" + minutes + ":" + seconds + meridiem);
+//
+// }
+//
+//
+// setInterval('clock()', 1000);
