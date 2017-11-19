1a7ba2bc70f280bccc86676dc525f0506f99282f
IsabelDePapel <isuchanek@gmail.com>
2017-11-18 20:23:35 -0800

    set up clock

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code here
+
+$(document).ready(() => {
+  const runClock = function runClock() {
+    const curTime = new Date();
+    $('#clock').html(curTime.toLocaleTimeString());
+  };
+});
