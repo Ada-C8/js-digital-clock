b7f40f203e7d4ea5417fa12164564b52d2f59877
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-11-19 15:58:16 -0800

    Bare requirements without css

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+
+// Update your clock to show the full date with the day of the week, and the month, day and year
+
+
+function clock() {
+let time = new Date();
+let h = time.getHours() % 12 || 12;
+let m = time.getMinutes();
+let s = time.getSeconds();
+let day = time.getDay();
+let month = time.getMonth();
+let weekDay = time.get
+let year = time.getYear();
+)
+
+// day of the week
+// month
+// day
+// year
+
+document.querySelectorAll('.clock')[0].innerHTML = placeHolder(h) + ":" + placeHolder(m) + ":" + placeHolder(s);
+
+function placeHolder(num) {
+  if (num < 10) {
+    num = '0' + num
+  }
+  return num;
+ }
+}
+
+setInterval(clock, 1000);
