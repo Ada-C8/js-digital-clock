e2a4a1ea57b7b124671ea5f487c357a653dfefd8
averikitsch <aekitsch@gmail.com>
2017-11-19 12:05:03 -0800

    refactoring

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
-
-var intervalID = window.setInterval(myClock, 1000);
-
-Number.prototype.pad = function(size) {
-  var s = String(this);
-  while (s.length < (size || 2)) {s = "0" + s;}
-  return s;
+const pad = function pad(n) {
+  if (n < 10) {
+    return (`0${n}`);
+  }
+  return n;
+};
+function myColor(secs) {
+  const hue = secs * 6;
+  const sat = 75;
+  const lig = 40;
+  return `hsl(${hue}, ${sat}%, ${lig}%)`;
 }
-
 function myClock() {
-  // let time = new Date().toLocaleTimeString();
-  // $('#clock').text(time);
-  let time = new Date();
-  let hour = time.getHours();
-  let min = time.getMinutes();
-  let seconds = time.getSeconds();
+  const time = new Date();
+  const weekDay = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+  };
+  const months = {
+    1: 'Jan',
+    2: 'Feb',
+    3: 'Mar',
+    4: 'Apr',
+    5: 'May',
+    6: 'Jun',
+    7: 'Jul',
+    8: 'Aug',
+    9: 'Sept',
+    10: 'Oct',
+    11: 'Nov',
+    12: 'Dec',
+  };
+  const hour = time.getHours();
+  const min = time.getMinutes();
+  const secs = time.getSeconds();
+  const month = months[time.getMonth() + 1];
+  const day = weekDay[time.getDay()];
 
-  $('#hour').text(hour);
-  $('#minutes').text(min.pad());
-  $('#seconds').text(seconds.pad());
+  const color = myColor(secs);
+  $('body').css('background-color', color);
+
+  $('#day').text(`${day}, ${month} ${time.getDate()}`);
+  $('#hour').text(hour).css('color', color);
+  $('#minutes').text(pad(min)).css('color', color);
+  $('#seconds').text(pad(secs)).css('color', color);
 }
-
-function myBackground() {
-
-}
+window.setInterval(myClock, 1000);
