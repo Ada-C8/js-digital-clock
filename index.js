5559c6f9b3051da8ff7d7b67769ea182b997960e
Maria McGrew <mcgmar1@outlook.com>
2017-11-19 23:21:10 -0800

    digital clock complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+$(document).ready(function() {
+  const digital = function digital() {
+    const time = new Date();
+    let hours = time.getHours().toString().length === 1 ? `0${time.getHours()}` : `${time.getHours()}`;
+    let minutes = time.getMinutes().toString().length === 1 ? `0${time.getMinutes()}` : `${time.getMinutes()}`;
+    let seconds = time.getSeconds().toString().length === 1 ? `0${time.getSeconds()}` : `${time.getSeconds()}`;
+    let clock = `${hours} : ${minutes} : ${seconds}`;
+$('#clock').text(clock)
+}
+
+setInterval(digital, 1000);
+});
