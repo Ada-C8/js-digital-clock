3b3ea606bb8a5669d66e4b19f2011ba16f192934
Tanisha <tanishahampden@gmail.com>
2017-11-19 19:03:06 -0800

    add display clock method

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // Your code here
 // new Date(year, month, date, hours, minutes, seconds, milliseconds);
 console.log(new Date());
+
+// .toLocaleTimeString - converts time to readable string
+// .toLocaleDateString() - converts date to readable string
+
+function display_clock() {
+  let time = new Date();
+  
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+
+  $(document).ready(()=>{
+    $('#clock').text(`${hours} : ${minutes} : ${seconds}`);
+  });
+}
+setInterval(display_clock, 1000);
