5d4086bf94e052a38ed2e04f81288e6bb8a2a8a2
Jessica Owens <vertige@gmail.com>
2017-11-19 18:37:15 -0800

    Cleaned up CSS a little, put clocks in time order

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /* Chose not to use Moments.js for practice, so one day I can appreciate Moments that much more */
 
 const locales = [
-  { name: 'Universal', lang: 'en-US', utc: { offset: 0, start: 12, end: 13 } }, // There is no 12 or 13 month, aka- never.
   { name: 'Seattle', lang: 'en-US', utc: { offset: -8, start: 3, end: 10 } },
-  { name: 'Cap Haitien', lang: 'ht-HT', utc: { offset: -5, start: 3, end: 10 } },
+  { name: 'Cap Haitien', lang: 'fr-HT', utc: { offset: -5, start: 3, end: 10 } }, // sadly, JS can't parse 'ht-HT' for Kreyol Ayisian
+  { name: 'Universal', lang: 'en-US', utc: { offset: 0, start: 12, end: 13 } }, // There is no 12 or 13 month, aka- never.
   { name: 'London', lang: 'en-GB', utc: { offset: 1, start: 3, end: 10 } },
   { name: 'Stuttgart', lang: 'de-DE', utc: { offset: 1, start: 3, end: 10 } },
   { name: 'Hong Kong', lang: 'zh-Hans-HK', utc: { offset: 8, start: 3, end: 10 } },
@@ -27,14 +27,14 @@ class DisplayClock {
 $(document).ready(() => {
   locales.forEach((loc) => {
     console.log(loc);
-    $('#dynamic-clocks').append(`<div>${loc.name}</div><div class='date'>${DisplayClock.displayDate(loc.lang)}</div><div class='clock ${loc.name}'></div>`);
+    $('#dynamic-clocks').append(
+      `<article class='clock'>
+      <div class='place'>${loc.name}</div>
+      <div class='time ${loc.name}'></div>
+      <div class='date'>${DisplayClock.displayDate(loc.lang)}</div>
+      </article>`);
     window.setInterval(() => {
-      $(`.${loc.name}`).html(DisplayClock.displayTime(loc.lang, loc.utc));
-    }, 1000);
+      $(`.${loc.name.split(' ')[0]}`).html(DisplayClock.displayTime(loc.lang, loc.utc));
+    }, 1000); // TODO: fix so class names aren't just first word
   });
 });
-
-//
-
-// $('#date').html(DisplayClock.displayDate());
-// $('#clock').html(DisplayClock.displayTime());
