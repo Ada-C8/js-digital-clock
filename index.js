eb5dc9c7c0da0e9047030de63a5177b79c8aa2f9
Kayla Ecker <The Email Associated with Your Github Account>
2017-11-20 09:12:53 -0800

    basic functionality complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+var clock = function() {
+  var now = new Date();
+  var h = tickTock(now.getHours());
+  var m = tickTock(now.getMinutes());
+  var s = tickTock(now.getSeconds());
+
+  var time = $('<p>' + h + ":" + m + ":" + s + '</p>');
+  $('#clock').empty();
+  $('#clock').append(time);
+};
+
+var tickTock = function(number) {
+  return (number < 10 ? '0' : '') + number;
+};
+
+
+$(document).ready(function() {
+
+  setInterval(clock, 1000);
+
+});
