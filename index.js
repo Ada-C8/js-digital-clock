e02856eabdd6afc2821676029680f0d6997350a9
murog <guillermina.muro@gmail.com>
2017-11-20 00:11:43 -0800

    working clock

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+$( document ).ready(function(){ // make arrow function
+  setInterval('updateClock()', 1000);
+});
+
+function updateClock(timeZone = null) {
+  const now = new Date();
+  console.log(now);
+  let currentHours = now.getHours();
+  let currentMinutes = now.getMinutes();
+  let currentSeconds = now.getSeconds();
+
+currentHours = (currentHours === 0) ? 12 : currentHours;
+
+const timeOfDay = (currentHours > 12) ? 'PM' : 'AM';
+
+currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+
+currentMinutes = (currentMinutes < 10) ? `0${currentMinutes}` : currentMinutes;
+
+currentSeconds = (currentSeconds < 10) ? `0${currentSeconds}` : currentSeconds;
+
+const currentTimeString = `${currentHours}:${currentMinutes}:${currentSeconds} ${timeOfDay}`
+
+
+$('#clock').html(currentTimeString);
+$('#date').html()
+}
