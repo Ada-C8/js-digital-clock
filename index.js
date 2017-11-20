4a47896d20da3d664bafbb6a94bdc244fd22a9bc
Amy Cash <amymcash@gmail.com>
2017-11-19 20:09:55 -0800

    Final

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+
+function addZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+let timer = function timer () {
+  let d = new Date();
+  let h = addZero(d.getHours());
+  let m = addZero(d.getMinutes());
+  let s = addZero(d.getSeconds());
+  let t = (h + ':' + m + ':' + s);
+  $("#clock").html(t);
+  // return t;
+};
+
+$(document).ready(function() {
+  setInterval('timer()', 1000);
+});
+
+// this version users the function to show
+// just the result
+// $(document).ready(function() {
+//   // $('#clock').append(timer());
+// })
 /* THE FOLLOWING WILL ADD
 THE THING IN THE APPEND TO
 THE PAGE */
@@ -59,148 +86,3 @@ with seconds in two digits  */
 //   let t = (h + ':' + m + ':' + s);
 //   $('#clock').append(t);
 // })
-
-function addZero(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
-}
-
-let timer = function timer () {
-  let d = new Date();
-  let h = addZero(d.getHours());
-  let m = addZero(d.getMinutes());
-  let s = addZero(d.getSeconds());
-  let t = (h + ':' + m + ':' + s);
-  $("#clock").html(t);
-  // return t;
-};
-$(document).ready(function() {
-  setInterval('timer()', 1000);
-});
-
-// this version users the function to show
-// just the result
-// $(document).ready(function() {
-//   // $('#clock').append(timer());
-// })
-// window.setInterval(timer(), 1000);
-console.log("outside the function");
-// setInterval(timer(), 1000);
-// The setInterval() method calls a function or evaluates an expression at specified intervals (in milliseconds).
-// setInterval('clock()', 1000);
-
-
-
-
-
-
-//
-// Your code here
-// const setDate = function setDate() {
-//   const now = new Date();
-//   const seconds = now.getSeconds();
-//   const mins = now.getMinutes();
-//   const hour = now.getHours();
-//
-//   setInterval(setDate, 1000);
-// });
-
-// $(document).ready(function() {
-//
-//   const setDate = function(setDate() {
-//     const now = new Date();
-//     const seconds = now.getSeconds();
-//     const mins = now.getMinutes();
-//     const hour = now.getHours();
-//     const date = seconds + mins + hour;
-//     // setInterval(setDate, 1000);
-//   });
-//
-//     $('#clock').append(date;
-//   });
-
-// });
-
-// $(document).ready(function ()) {
-//   $('#clock').append("hi there");
-// }
-//
-// $(document).ready(function() {
-
-// (hours + ":" + minutes + ":" + seconds + meridiem);
-// </script>
-
-// $(document).ready(function() {
-//   let interval = setInterval(myTimer, delay[1000]);
-//
-//   function myTimer() {
-//     let d = new Date();
-//     let t = d.getSeconds();
-//   }
-//   // let d = new Date();
-//   // let h = d.getHours();
-//   // let m = d.getMinutes();
-//   // let s = d.getSeconds();
-//   // let t = h + ':' + m + ':' + s;
-//   $('#clock').append(interval);
-//
-// });
-
-//
-//
-// var clock = function clock() {
-//
-// //Save the times in variables
-//
-// var today = new Date();
-//
-// var hours = today.getHours();
-// var minutes = today.getMinutes();
-// var seconds = today.getSeconds();
-//
-//
-// //Set the AM or PM time
-//
-// if (hours >= 12){
-//   meridiem = " PM";
-// }
-// else {
-//   meridiem = " AM";
-// }
-//
-//
-// //convert hours to 12 hour format and put 0 in front
-// if (hours>12){
-// 	hours = hours - 12;
-// }
-// else if (hours===0){
-// 	hours = 12;
-// }
-//
-// //Put 0 in front of single digit minutes and seconds
-//
-// if (minutes<10){
-// 	minutes = "0" + minutes;
-// }
-// else {
-// 	minutes = minutes;
-// }
-//
-// if (seconds<10){
-// 	seconds = "0" + seconds;
-// }
-// else {
-// 	seconds = seconds;
-// }
-//
-// document.getElementById("clock").innerHTML = (hours + ":" + minutes + ":" + seconds + meridiem);
-//
-//
-//
-// }; //ends clock function
-
-// console.log("outside the function")
-// setInterval(clock, 1000);
-// setInterval('clock()', 1000);
