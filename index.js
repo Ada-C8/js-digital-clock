4fc81639b2f82f07bd2cc8e9031dea5c5156d7db
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-11-19 16:23:45 -0800

    Add month, date, year and weekday to display

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 
 // Update your clock to show the full date with the day of the week, and the month, day and year
+$(document).ready(function() {
 
-
-function clock() {
-let time = new Date();
-let h = time.getHours() % 12 || 12;
-let m = time.getMinutes();
-let s = time.getSeconds();
-let day = time.getDay();
-let month = time.getMonth();
-let weekDay = time.get
-let year = time.getYear();
-)
-
-// day of the week
-// month
-// day
-// year
-
-document.querySelectorAll('.clock')[0].innerHTML = placeHolder(h) + ":" + placeHolder(m) + ":" + placeHolder(s);
-
-function placeHolder(num) {
-  if (num < 10) {
-    num = '0' + num
+  function placeHolder(num) {
+    if (num < 10) {
+      num = '0' + num;
+    }
+    return num;
   }
-  return num;
- }
-}
 
-setInterval(clock, 1000);
+  function clock() {
+    let time = new Date();
+    let h = time.getHours() % 12 || 12;
+    let m = time.getMinutes();
+    let s = time.getSeconds();
+
+    const monthsArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let month = monthsArray[time.getMonth()];
+
+    let date = time.getDate();
+    let year = time.getFullYear();
+
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let dayName = days[time.getDay()];
+
+    let text = `It is ${ dayName } <br> ${month} ${date}, ${year} <br> The time is ` + placeHolder(h) + ":" + placeHolder(m) + ":" + placeHolder(s);
+
+    $('.clock').html(text);
+
+// I found this online, and was not really sure everything that it did...
+    // document.querySelectorAll('.clock')[0].innerHTML = placeHolder(h) + ":" + placeHolder(m) + ":" + placeHolder(s);
+  }
+
+  setInterval(clock, 1000);
+
+});
