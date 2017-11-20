57721a3c252dc36ce9b50d74a6b3e0a00db99c1d
Sara Frandsen <sara.frandsen@gmail.com>
2017-11-19 18:19:45 -0800

    add comments on how things work

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
-// let $dOut = $('#date')
+// for CSS
 let $hours = $('#hours');
 let $minutes = $('#minutes');
 let $seconds = $('#seconds');
 let $amPm = $('#ampm');
-// let months = [
-//   'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
-// ];
-//
-// let days = [
-//   'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
-// ];
 
-const update = function update(){
+const updateClock = function updateClock(){
+  // initialize new Date object
   let date = new Date();
 
-  let ampm = date.getHours() < 12 ? 'AM' : 'PM';
+  let amPm = date.getHours();
+  // if the hour is less than 12, AM
+  if (amPm < 12) {
+    amPm = 'AM';
+    // if the hour is less than 12, PM
+  } else if (amPm > 12) {
+    amPm = 'PM';
+  };
 
   let hours = date.getHours()
+  // if hour is 0, midnight (12)
   if (hours == 0) {
     hours = 12;
+  // if hour is > 12, -12 so is not military time
   } else if (hours != 0 && hours > 12) {
     hours = hours - 12;
   } else {
+  // no change
     hours = hours;
   };
 
   let minutes = date.getMinutes()
+  // add leading 0 so always two digits
   if (minutes < 10) {
     minutes = '0' + date.getMinutes();
   };
 
   let seconds = date.getSeconds()
+  // add leading 0 so always two digits
   if (seconds < 10) {
     seconds = '0' + seconds;
   };
-  //
-  // let dayOfWeek = days[date.getDay()];
-  // let month = months[date.getMonth()];
-  // let day = date.getDate();
-  // let year = date.getFullYear();
-  //
-  // let dateString = dayOfWeek + ', ' + month + ' ' + day + ', ' + year;
 
-  // $dOut.text(dateString);
+  // jQuery $ for output
+  // assigned at very top to IDs
+  // add text to fields with matching ID
   $hours.text(hours);
   $minutes.text(minutes);
   $seconds.text(seconds);
-  $amPm.text(ampm);
+  $amPm.text(amPm);
 }
 
-update();
-window.setInterval(update, 1000);
+updateClock();
+// set interval to 1000 milliseconds (1 second)
+// 'window' is current window
+// did not work with 'document'
+window.setInterval(updateClock, 1000);
