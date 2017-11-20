13cb6d5c647df59d60e2e279ad0f073380ce580b
enigmagnetic <lauren.cardella@gmail.com>
2017-11-20 03:56:30 -0800

    Add script links and basic structure to index HTML. Build clock function.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+const clock = function clock() {
+  let currentTime = new Date();
+  let currentMonth = MONTHS[currentTime.getMonth()];
+  let currentDay = DAYS[currentTime.getDay()];
+  let currentHour = currentTime.getHours();
+  let currentMinute = currentTime.getMinutes();
+  let currentSecond = currentTime.getSeconds();
+
+$('#date').html('<p>' + currentDay + '</p>' + '<p>' + currentMonth + ' ' + currentTime.getDate() + ', ' + currentTime.getFullYear() + '</p>');
+
+  $('#time').html('<p>' + currentTime.getHours() + ':' + currentTime.getMinutes() + ':' + currentTime.getSeconds() + '</p>');
+};
+
+setInterval(clock, 1000);
