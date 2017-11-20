2d5fd41f4aa3d22b34b23173f1c69d76e666fda0
Julia <julia.meier@gmail.com>
2017-11-19 18:16:37 -0800

    prints to console

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+setInterval(function(){currentClock()}, 1000);
+
+  function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+  }
+
+  function currentClock() {
+      var currentDate = new Date();
+      var currentHours = currentDate.getHours() == 0 ? 12 : currentDate.getHours() > 12 ? currentDate.getHours() - 12 : currentDate.getHours();
+      var currentMinutes = addZero(currentDate.getMinutes());
+      var currentSeconds = addZero(currentDate.getSeconds());
+      //< 10 ? '0' + date.getSeconds() : currentDate.getSeconds();
+      var currentAMPM = currentDate.getHours() < 12 ? 'AM': 'PM';
+      var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + currentAMPM;
+      $("#clock").html(currentTimeString);
+}
